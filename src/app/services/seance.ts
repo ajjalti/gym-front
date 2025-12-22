82d54aeeb474ef62b9ca137f0c155fc3cc7f1e98
KhaoulaBouziane1 <145008864+KhaoulaBouziane1@users.noreply.github.com>
2025-12-22 16:21:10 +0100

    22/12/2025khaoula

--- src/app/services/seance.ts
+++ src/app/services/seance.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Seance {
-  
+  private http = inject(HttpClient);
+  private api = environment.api + 'seances';
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.api);
+  }
+
+  save(seance: any): Observable<any> {
+    return this.http.post(this.api, seance);
+  }
+
+  delete(id: number | string): Observable<any> {
+    return this.http.delete(`${this.api}/${id}`);
+  }
 }
