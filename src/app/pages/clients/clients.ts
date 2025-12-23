f9dd28f139d54e95abc6fdb30601cc0abb4ce607
ajjalti <ahmed.ajjalti@gmail.com>
2025-12-23 09:10:50 +0100

    add some values display for client

--- src/app/pages/clients/clients.ts
+++ src/app/pages/clients/clients.ts
@@ -113,7 +113,8 @@ export class Clients implements OnInit{
     calendar: this.bypass(`<rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line>`),
     dollar: this.bypass(`<line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>`),
     check: this.bypass(`<polyline points="20 6 9 17 4 12"></polyline>`),
-    alert: this.bypass(`<circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line>`)
+    alert: this.bypass(`<circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line>`),
+     phone: this.bypass(`<path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l2.27-2.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>`)
   };
 
   constructor(private clientservice:Client){}
